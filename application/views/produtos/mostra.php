63089b063bff9ce26f97ca4168b8aac066d6d62a
Valdir Luiz <valdirluiz@localhost.localdomain>
2014-03-16 11:42:53 -0300

    Alterando venda de produto

--- application/views/produtos/mostra.php
+++ application/views/produtos/mostra.php
@@ -10,6 +10,32 @@
 	<h1>Nome: <?= $produto['nome']?></h1>
 	<p>Descrição: <?= auto_typography(html_escape($produto['descricao']))?></p>
 	<p>Preço: <?= $produto['preco']?></p>
+	
+	<!-- Formulario de compra -->
+	<h2>Compre agora mesmo!</h2>
+	<?php
+    echo form_open("vendas/nova");
+	
+    	echo form_hidden("produto_id", $produto["id"]);
+	    
+        echo form_label("Data de entrega" , "data_de_entrega");
+	    echo form_input(array(
+	        "name" => "data_de_entrega",
+	        "class" => "form-control",
+	        "id" => "data_de_entrega",
+	        "maxlength" => "255",
+	        "value" => ""
+	    ));
+	    
+	    echo form_button(array(
+	        "class" => "btn btn-primary",
+	        "content" => "Comprar",
+	        "type" => "submit"
+	    ));
+
+    echo form_close();
+
+    ?>
 	</div>
 </body>
 </html>
