a311f389b0410ef317af130213c7fc0b365e00dd
Valdir <arnholdvaldir@gmail.com>
2014-03-19 16:09:46 -0300

    Update login.php
    Identar código

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -4,7 +4,7 @@ class Login extends CI_Controller {
 	
 	
 	public function autenticar(){
-			$this->load->model("usuarios_model");
+		$this->load->model("usuarios_model");
         	$email = $this->input->post("email");
         	$senha = md5($this->input->post("senha"));
         	$usuario = $this->usuarios_model->buscaPorEmailESenha($email, $senha);
